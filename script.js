66e586b3ed590d2a2936f5d247dea52720dc0b12
front42 <altaifront@gmail.com>
2024-11-28 09:03:58 +0300

    feat: add toggle fullscreen

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.fullscreen').addEventListener('click', toggleFullscreen);
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
