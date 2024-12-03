f7a2b50a4acd81c1c67797eef2e2288b7b447816
front42 <altaifront@gmail.com>
2024-12-03 15:19:49 +0300

    feat: add notes/letters switching

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # <img src="./assets/favicon.png" alt="Music Clef" height="20"/>virtual-piano
-Thanks for your attention to this practice with JS events.  
+Thanks for your attention to this practice with DOM, JS events and Fullscreen API.  
 Click & enjoy playing your own song on piano keys or keyboard letters:  
 ***https://front42.github.io/virtual-piano***  
 
--- script.js
+++ script.js
@@ -14,3 +14,20 @@ document.addEventListener('keydown', (event) => {
     toggleFullscreen();
   }
 });
+
+const btnContainer = document.querySelector('.btn-container');
+const btns = document.querySelectorAll('.btn');
+const piano = document.querySelector('.piano');
+const pianoKeys = piano.querySelectorAll('.piano-key');
+
+btnContainer.addEventListener('click', (event) => {
+  const targetBtn = event.target.closest('.btn');
+  if (!targetBtn) return;
+  btns.forEach((btn) => btn.classList.remove('btn-active'));
+  targetBtn.classList.add('btn-active');
+  if (targetBtn.classList.contains('btn-letters')) {
+    pianoKeys.forEach((key) => key.classList.add('piano-key-letter'));
+  } else {
+    pianoKeys.forEach((key) => key.classList.remove('piano-key-letter'));
+  }
+});
