cd61c2d656dc02191bdd532d9e84e6c628f16d98
front42 <altaifront@gmail.com>
2024-12-05 09:34:23 +0300

    feat: add audio playing

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-document.querySelector('.fullscreen').addEventListener('click', toggleFullscreen);
-
-function toggleFullscreen() {
-  if (!document.fullscreenElement) {
-    document.documentElement.requestFullscreen();
-  } else if (document.exitFullscreen) {
-    document.exitFullscreen();
+document.addEventListener('click', (event) => {
+  if (event.target.closest('.btn')) {
+    switchNotesLetters(event);
+  } else if (event.target.closest('.fullscreen')) {
+    toggleFullscreen();
   }
-}
+});
+
+document.addEventListener('mousedown', (event) => {
+  if (event.target.closest('.piano-key')) {
+    document.addEventListener('mouseover', playAudio);
+    playAudio(event);
+  }
+});
+
+document.addEventListener('mouseout', (event) => {
+  event.target.classList.remove('piano-key-active');
+});
+
+document.addEventListener('mouseup', (event) => {
+  event.target.classList.remove('piano-key-active');
+  document.removeEventListener('mouseover', playAudio);
+});
 
 document.addEventListener('keydown', (event) => {
-  if (event.code === 'F11') {
+  if (event.repeat) return;
+  if (document.querySelector(`[data-letter="${event.code.slice(3)}"]`)) {
+    playAudio(event);
+  } else if (event.code === 'F11') {
     event.preventDefault();
     toggleFullscreen();
   }
 });
 
-const btnContainer = document.querySelector('.btn-container');
-const btns = document.querySelectorAll('.btn');
-const piano = document.querySelector('.piano');
-const pianoKeys = piano.querySelectorAll('.piano-key');
+document.addEventListener('keyup', (event) => {
+  const key = document.querySelector(`[data-letter="${event.code.slice(3)}"]`);
+  if (!key) return;
+  key.classList.remove('piano-key-active');
+});
 
-btnContainer.addEventListener('click', (event) => {
+const btns = document.querySelectorAll('.btn');
+const pianoKeys = document.querySelectorAll('.piano-key');
+
+const switchNotesLetters = (event) => {
   const targetBtn = event.target.closest('.btn');
-  if (!targetBtn) return;
   btns.forEach((btn) => btn.classList.remove('btn-active'));
   targetBtn.classList.add('btn-active');
   if (targetBtn.classList.contains('btn-letters')) {
@@ -30,4 +50,26 @@ btnContainer.addEventListener('click', (event) => {
   } else {
     pianoKeys.forEach((key) => key.classList.remove('piano-key-letter'));
   }
-});
+};
+
+const toggleFullscreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+};
+
+const playAudio = (event) => {
+  const key =
+    event.target.closest('.piano-key') ||
+    document.querySelector(`[data-letter="${event.code?.slice(3)}"]`);
+  if (!key || key.classList.contains('piano-key-active')) return;
+  key.classList.add('piano-key-active');
+
+  const note = key.dataset.note;
+  const src = `./assets/audio/${note}.mp3`;
+  const audio = new Audio(src);
+  audio.currentTime = 0;
+  audio.play();
+};
